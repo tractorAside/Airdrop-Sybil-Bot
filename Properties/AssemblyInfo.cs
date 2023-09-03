bcde0aa6c1fe165b5143b8862852781ff6ad7dde
tractorAside <136760239+tractorAside@users.noreply.github.com>
2023-09-03 18:35:17 +0000

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Blockchain Airdrop Bot")]
+[assembly: AssemblyDescription("Automated coin transfers, token swaps, token transfers and logical transaction routes. Ultimate solution for simulating blockchain events on EVM chains.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Blockchai nAirdrop Bot")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("75B00600-52B5-4DED-BBDA-1C49D76F57BC")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
